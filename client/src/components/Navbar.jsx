f10cd4be004cfa34c6e515668753acd23bc32204
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-05-29 19:24:42 -0400

    background image for nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const AppNavbar = () => {
     <>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
-          <Navbar.Brand as={Link} to='/home' className='brand'>
+          <Navbar.Brand as={Link} to='/' className='brand'>
             PlanPro
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
