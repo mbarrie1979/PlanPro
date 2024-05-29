891fce468c667cb1b6f2387ec19f28d1bb472c90
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-05-29 19:49:10 -0400

    changed font to Abril

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const AppNavbar = () => {
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                <Nav.Link onClick={() => setShowModal(true)} className="login">Login/Sign Up</Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
