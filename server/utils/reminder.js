7abdf7338ba52b88db8cdc8961ef13fcea51f4db
Milman <milindmjoshi@live.com>
2024-06-05 18:18:19 -0400

    Add .env file for holding API KEY secret

--- server/utils/reminder.js
+++ server/utils/reminder.js
@@ -3,12 +3,13 @@ const { User, Session, Conference } = require('../models');
 
 // Iterate over all users and sessions, and send reminders when due
  sendReminder = async()=>{
+    console.log("Reminder Check Started");
     const users = await User.find();
     //Note: Cannot USE forEach with an async clause as forEach run a callback and async code does not wait. Therefore, for loop is used. 
     //users.forEach(async(user)=> await sendUserReminder(user));
     for (let user of users){
         // Get all the user sessions and send reminders for each session if within 15 minutes of start time
-        //console.log("*** In send user reminder, User is: " + user.username);
+        console.log("*** In send user reminder, User is: " + user.username);
         //console.log("In send user reminder, User session count: " + user.savedSessions.length);
         if (user.getEmailReminders){
             for (let sessionId of user.savedSessions){
@@ -27,10 +28,11 @@ const { User, Session, Conference } = require('../models');
                 let currentDate = new Date();
                 //console.log("Current Date: " + currentDate);
                 // If difference between session date and current date is within 15 minutes , 15x60x1000 = 
-                if (sessionDate.getTime() - currentDate.getTime() <=900000){
+                let timeDifference = sessionDate.getTime() - currentDate.getTime();
+                if (timeDifference > 0 && timeDifference <=900000){
                     // send reminder if session within 15 minutes of starting
                     const msg = {
-                        to: 'milindmjoshi@live.com', // Change to your recipient
+                        to: `${user.email}`, //
                         from: 'mjoshimd@gmail.com', // Change to your verified sender
                         subject: `Session ${session.name} will begin in 15 minutes`,
                         text: `Session ${session.name} will begin in 15 minutes in room ${session.room}. Hope you enjoy it.    PlanPro Admin`,
